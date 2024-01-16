716b583d9e11f55a5749ee830c9f67703f80b754
bovem <av_nish@outlook.com>
2024-01-17 01:05:49 +0530

    Added subcommand summarize to generate a draft bragging documents usind large language models

--- utils/bragDocument.go
+++ utils/bragDocument.go
@@ -7,45 +7,23 @@ The document should be written in a formal tone as it has to be shared
 with my manager and stakeholders and it impacts my personal growth.
 Omit the fields you think don't have enough information in journals.
 
-The document should be in the following format:
+The document should be in the following format, the project names and content
+mentioned in following format is just an example don't use it in the final output
+Use the pointers provided in the format below and omit them in the final output:
+
 # Work Accomplishments
 Insert the list of personal work done in great detail, mentioning important
 hyperlinks like Jira tickets, pull request links, and other relevant document links
-. It should be grouped by projects like the following
-
-## AIS Maritime Traffic Dashboard
-### Completed
-* Created a scraper to fetch the data for current maritime traffic.
-* Integrated PostgresDB to store the daily data for maritime traffic.
-* Created a GraphQL API to access the data from different services.
-
-### In progress
-* Working on a Grafana Dashboard to visualize the maritime traffic.
-
-### Todos
-* Exploring methodologies to improve scraping performance. 
-
-## Work Project 2
-### Completed
-* Completed tasks
-
-### In progress
-* In Progress tasks
-
-### Todos
-* Task not started yet but planning to be
+. It should be grouped by projects.
 
 Emphasize the following points
 * If I designed or built something from scratch
 * Useful insights during the design or coding process
 * Impact of individual projects
-* Using numbers to show impact like reduced response time by X%, decreased cloud costs by X%\
+* Using numbers to show impact like reduced response time by X%, decreased cloud costs by X%.
 
 # Collaboration and Mentorship
-Write about collaboration and mentorship work done by me in bullet points like the following:
-
-* Helped intern with a task he was stuck on
-* Worked with multiple teams to resolve a crucial production bug.
+Write about collaboration and mentorship work done by me in bullet points.
 
 Emphasize the following points
 * Write about each point in great detail supporting it with hyperlinks when necessary
@@ -57,14 +35,7 @@ Emphasize the following points
 * Giving talks or conducting workshops and hackathons internally
 
 # Design and Documentation
-Write about design and documentation efforts done in the following format
-
-## Project 1
-* Designed API services for Project 1
-
-## Project 2
-* Improved documentation for Project 2 by adding information for new features.
-* Added a FAQ section for Project 2 with the questions frequently asked by our partners and customers.
+Write about design and documentation efforts.
 
 # Company Building
 Write in bullet points about the interviewing and recruiting process contributions from my side.
@@ -81,4 +52,6 @@ the work. Also, write about
 * Talks and event participation
 * Contributing to OpenSource projects
 * Recognition from any other part of the industry. Like awards.
+
+Additional information about personal projects and blogs will be mentioned after the journal.
 `
